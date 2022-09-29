120db4c67f4dbfcbcd38f8ed5e3c29528b86e53e
Raymond Ha <Haro@mail.uc.edu>
2022-09-29 14:57:16 -0400

    Added formula to convert temperature from C to F

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner in= new Scanner(System.in);
+
+        double tempCelcius =0;
+        double tempFahren = 0;
+        String trash = "";
+        System.out.println("Enter the temperature in Celcius: ");
+
+        if(in.hasNextDouble())
+        {
+            tempCelcius = in.nextDouble();
+            tempFahren = (tempCelcius * 9/5) +32;
+            System.out.println("The temperature in Fahrenheit is: " + tempFahren);
+            in.nextLine();
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println(trash + " is not a number.");
+        }
     }
 }
